578c45c3075d196f7eaa4c23c95fe35bbbf50e80
Diogo Andrade <diogo.andrade@mind.pt>
2023-10-21 15:18:55 +0100

    Fixed issues with files moving Camera now follows player

---
+++ CameraTrack.cs
@@ -0,0 +1,100 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+
+namespace OpenTKBase
+{
+    public class CameraTrack : Component
+    {
+        public PlayerBike       player;
+        private Vector3         offset = new Vector3(0.0f, 2.5f, -5.0f);
+        private float           followSpeed = 0.05f;
+
+        public override void Start()
+        {
+        }
+
+        public override void Update()
+        {
+            Vector3 targetPos = player.transform.position + 
+                                offset.X * player.transform.right +
+                                offset.Y * player.transform.up +
+                                offset.Z * player.transform.forward;
+
+            Vector3 currentPos = transform.position;
+
+            transform.position = currentPos + (targetPos - currentPos) * followSpeed;
+
+            Vector3 dir = ((player.transform.position + player.transform.up * 2.0f) - currentPos).Normalized();
+            Quaternion currentRotation = transform.rotation;
+            Quaternion targetRotation = LookRotation(dir, Vector3.UnitY);
+
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, followSpeed);
+        }
+
+        public static Quaternion LookRotation(Vector3 forward, Vector3 up)
+        {
+            forward = -Vector3.Normalize(forward);
+
+            Vector3 right = Vector3.Normalize(Vector3.Cross(up, forward));
+            up = Vector3.Cross(forward, right);
+
+            float m00 = right.X;
+            float m01 = right.Y;
+            float m02 = right.Z;
+            float m10 = up.X;
+            float m11 = up.Y;
+            float m12 = up.Z;
+            float m20 = forward.X;
+            float m21 = forward.Y;
+            float m22 = forward.Z;
+
+            float num8 = (m00 + m11) + m22;
+
+            if (num8 > 0f)
+            {
+                float num0 = (float)Math.Sqrt(num8 + 1f);
+                float num2 = 0.5f / num0;
+                return new Quaternion(
+                    (m12 - m21) * num2,
+                    (m20 - m02) * num2,
+                    (m01 - m10) * num2,
+                    num0 * 0.5f
+                );
+            }
+
+            if ((m00 >= m11) && (m00 >= m22))
+            {
+                float num7 = (float)Math.Sqrt(((1f + m00) - m11) - m22);
+                float num4 = 0.5f / num7;
+                return new Quaternion(
+                    0.5f * num7,
+                    (m01 + m10) * num4,
+                    (m02 + m20) * num4,
+                    (m12 - m21) * num4
+                );
+            }
+
+            if (m11 > m22)
+            {
+                float num6 = (float)Math.Sqrt(((1f + m11) - m00) - m22);
+                float num3 = 0.5f / num6;
+                return new Quaternion(
+                    (m10 + m01) * num3,
+                    0.5f * num6,
+                    (m21 + m12) * num3,
+                    (m20 - m02) * num3
+                );
+            }
+
+            float num5 = (float)Math.Sqrt(((1f + m22) - m00) - m11);
+            float num = 0.5f / num5;
+            return new Quaternion(
+                (m20 + m02) * num,
+                (m21 + m12) * num,
+                0.5f * num5,
+                (m01 - m10) * num
+            );
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -194,15 +194,6 @@ namespace SDLBase
             env.Set("FogParams", new Vector2(0.0f, 80.0f));
             env.Set("FogColor", Color.Black);
 
-            // Create camera
-            GameObject cameraObject = new GameObject();
-            Camera camera = cameraObject.AddComponent<Camera>();
-            camera.transform.position = startPos + Vector3.UnitY * 2.5f - Vector3.UnitX * 5;
-            camera.transform.rotation = Quaternion.FromEulerAngles(0.0f, MathF.PI / 2.0f, 0.0f);
-            camera.ortographic = false;
-            FirstPersonController fps = cameraObject.AddComponent<FirstPersonController>();
-            fps.rotation = new Vector2(0.0f, -MathF.PI / 2.0f);
-
             // Create player character
             var motorbike_texture = Resources.LoadTexture("Textures/motorbike.png", OpenTK.Graphics.OpenGL.TextureWrapMode.Clamp, OpenTK.Graphics.OpenGL.TextureMinFilter.Nearest, false);
             var player_sprite = Resources.CreateSprite("bike", motorbike_texture, new Vector2(0.5f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f), 90);
@@ -214,7 +205,18 @@ namespace SDLBase
             playerCharacter.transform.rotation = Quaternion.FromEulerAngles(0.0f, -MathF.PI / 2.0f, 0.0f);
             SpriteRenderer sr = playerCharacter.AddComponent<SpriteRenderer>();
             sr.sprite = player_sprite;
-            playerCharacter.AddComponent<PlayerBike>();
+            var pb = playerCharacter.AddComponent<PlayerBike>();
+
+            // Create camera
+            GameObject cameraObject = new GameObject();
+            Camera camera = cameraObject.AddComponent<Camera>();
+            camera.transform.position = startPos + Vector3.UnitY * 2.5f - Vector3.UnitX * 5;
+            camera.transform.rotation = Quaternion.FromEulerAngles(0.0f, MathF.PI / 2.0f, 0.0f);
+            camera.ortographic = false;
+            var track = cameraObject.AddComponent<CameraTrack>();
+            track.player = pb;
+            //FirstPersonController fps = cameraObject.AddComponent<FirstPersonController>();
+            //fps.rotation = new Vector2(0.0f, -MathF.PI / 2.0f);
 
             // Create pipeline
             RPCortex renderPipeline = new RPCortex();
